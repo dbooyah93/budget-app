0b11d6e339a827bf7fc161d61d234e0b1ef9d30c
David Boyea <davidmcruz1125@gmail.com>
2022-02-23 00:23:02 -0500

    Styles numbers and label for better UX

--- src/components/ESSR.vue
+++ src/components/ESSR.vue
@@ -1,12 +1,12 @@
 <template>
     <div class="essr">
         <div class="row">
-            <p class="col">Earned: {{earned}}</p>
-            <p class="col">Saved: {{saved}}</p>
+            <div class="col d-flex justify-content-between"><p>Earned:</p> <p>{{earned}}</p></div>
+            <div class="col d-flex justify-content-between"><p>Saved:</p> <p>{{saved}}</p></div>
         </div>
         <div class="row">
-            <p class="col">Spent: {{spent}}</p>
-            <p class="col">Remaining: {{remaining}}</p>
+            <div class="col d-flex justify-content-between"><p>Spent:</p> <p>{{spent}}</p></div>
+            <div class="col d-flex justify-content-between"><p>Remaining:</p> <p>{{remaining}}</p></div>
         </div>
     </div>
 </template>
