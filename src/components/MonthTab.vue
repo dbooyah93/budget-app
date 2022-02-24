7b6415eae9d8f2094b722988fbeaab405864d86a
David Boyea <davidmcruz1125@gmail.com>
2022-02-23 23:27:49 -0500

    Props implemented over Month Tabs with example information put into place

--- src/components/MonthTab.vue
+++ src/components/MonthTab.vue
@@ -1,5 +1,10 @@
 <template>
-    <b-tab title="January" active><b-card-text>{{name}}</b-card-text></b-tab>
+    <b-tab :title=name active>
+        <b-card-header>{{name}}</b-card-header>
+        <b-card-text>
+            <b-table hover :items="items" :fields="fields"></b-table>
+        </b-card-text>
+    </b-tab>
 </template>
 
 <script>
@@ -8,6 +13,27 @@ export default {
     props: ["name"],
     data () {
         return {
+            fields: [
+                {
+                    key: 'description',
+                    sortable: true,
+                },
+                {
+                    key: 'expence',
+                    sortable: true,
+                },
+                {
+                    key: 'date',
+                    sortable: true,
+                },
+                {
+                    key: 'category',
+                    sortable: true,
+                }
+            ],
+            items: [
+                { description: 'Milk and eggs from Walmart', expence: 9.39, date: "1/2/2022", category: "Groceries"}
+            ]
         }
     },
 
--- src/components/MonthsInReview.vue
+++ src/components/MonthsInReview.vue
@@ -2,17 +2,17 @@
     <b-card no-body >
         <b-tabs pills card vertical>
             <month-tab :name="'January'"/>
-            <b-tab title="Feburary" ><b-card-text>Feburary</b-card-text></b-tab>
-            <b-tab title="March" ><b-card-text>March</b-card-text></b-tab>
-            <b-tab title="April" ><b-card-text>April</b-card-text></b-tab>
-            <b-tab title="May" ><b-card-text>May</b-card-text></b-tab>
-            <b-tab title="June" ><b-card-text>June</b-card-text></b-tab>
-            <b-tab title="July" ><b-card-text>July</b-card-text></b-tab>
-            <b-tab title="August" ><b-card-text>August</b-card-text></b-tab>
-            <b-tab title="September" ><b-card-text>September</b-card-text></b-tab>
-            <b-tab title="October" ><b-card-text>October</b-card-text></b-tab>
-            <b-tab title="November" ><b-card-text>November</b-card-text></b-tab>
-            <b-tab title="December" ><b-card-text>December</b-card-text></b-tab>
+            <month-tab :name="'Feburary'"/>
+            <month-tab :name="'March'"/>
+            <month-tab :name="'April'"/>
+            <month-tab :name="'May'"/>
+            <month-tab :name="'June'"/>
+            <month-tab :name="'July'"/>
+            <month-tab :name="'August'"/>
+            <month-tab :name="'September'"/>
+            <month-tab :name="'October'"/>
+            <month-tab :name="'November'"/>
+            <month-tab :name="'December'"/>
             <b-tab title="Calculator" ><b-card-text>Calculator</b-card-text></b-tab>
         </b-tabs>
     </b-card>
