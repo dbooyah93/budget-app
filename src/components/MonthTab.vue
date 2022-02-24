6d98fbbcc1d138447c9f8056959a3eb563efd227
David Boyea <davidmcruz1125@gmail.com>
2022-02-23 23:07:18 -0500

    New component for individual months

--- src/components/MonthTab.vue
+++ src/components/MonthTab.vue
@@ -6,6 +6,7 @@
 <script>
 export default {
     name: 'month-tab',
+    props: ["name"],
     data () {
         return {
         }
--- src/components/MonthsInReview.vue
+++ src/components/MonthsInReview.vue
@@ -1,7 +1,7 @@
 <template>
     <b-card no-body >
         <b-tabs pills card vertical>
-            <b-tab title="January" active><b-card-text>January</b-card-text></b-tab>
+            <b-tab title="January" active><month-tab :name="'January'"/></b-tab>
             <b-tab title="Feburary" ><b-card-text>Feburary</b-card-text></b-tab>
             <b-tab title="March" ><b-card-text>March</b-card-text></b-tab>
             <b-tab title="April" ><b-card-text>April</b-card-text></b-tab>
@@ -13,18 +13,19 @@
             <b-tab title="October" ><b-card-text>October</b-card-text></b-tab>
             <b-tab title="November" ><b-card-text>November</b-card-text></b-tab>
             <b-tab title="December" ><b-card-text>December</b-card-text></b-tab>
-            <b-tab title="Calculator" ><b-footer-text>Calculator</b-footer-text></b-tab>
+            <b-tab title="Calculator" ><b-card-text>Calculator</b-card-text></b-tab>
         </b-tabs>
     </b-card>
 </template>
 
 <script>
+import MonthTab from "./MonthTab.vue";
 export default {
     name: 'months-in-review',
     data () {
         return {
-
         }
     },
+    components: {MonthTab}
 }
 </script>
