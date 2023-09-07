a61d593afb0e3c48796716cbf54d250799353e03
dbooyah93 <davidmcruz1125@gmail.com>
2023-09-07 12:59:49 -0500

    Update seed query to 'IF NOT EXISTS'

--- database/seed.js
+++ database/seed.js
@@ -8,18 +8,19 @@ let connection = mysql.createConnection({
     database : 'test_schema'
 });
 
-const createTable = "CREATE TABLE `test_schema`.`expenses` (`id` INT NOT NULL AUTO_INCREMENT,`Description` VARCHAR(300) NULL,`Date added` TIMESTAMP NOT NULL,`Date updated` TIMESTAMP NULL,`Date deleted` TIMESTAMP NULL,`Date purchased` DATE NULL,`Category` INT NULL,`Cost` DECIMAL(19,4) NULL,PRIMARY KEY (`id`),UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE);";
+const createTable = "CREATE TABLE IF NOT EXISTS `test_schema`.`expenses` (`id` INT NOT NULL AUTO_INCREMENT,`Description` VARCHAR(300) NULL,`Date added` TIMESTAMP NOT NULL,`Date updated` TIMESTAMP NULL,`Date deleted` TIMESTAMP NULL,`Date purchased` DATE NULL,`Category` INT NULL,`Cost` DECIMAL(19,4) NULL,PRIMARY KEY (`id`),UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE);";
 
 connection.connect();
 connection.on('error', (err)=>{
-    console.log(err);
+    console.log('General error:' + err);
 });
 
 let createTableCB = function (err, results) {
     if (err) {
-        console.error(err);
+        console.error("Create table error:" + err);
     }
     if (results) {
+        console.log("Create table results: ");
         console.log(results);
     }
 };
